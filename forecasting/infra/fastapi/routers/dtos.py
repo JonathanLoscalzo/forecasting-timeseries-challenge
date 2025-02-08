6d69275a512ff4f5678e3d4b28a2b7066ded2399
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2025-02-08 13:59:40 -0300

    chore: add example to openapi docs

--- forecasting/infra/fastapi/routers/dtos.py
+++ forecasting/infra/fastapi/routers/dtos.py
@@ -3,6 +3,8 @@ from typing import Dict
 
 from pydantic import BaseModel, Field
 
+from forecasting.const import EXAMPLE_INPUT
+
 
 class Item(BaseModel):
     date: date
@@ -13,3 +15,5 @@ class Data(BaseModel):
     historical: Dict[date, float]
     start_date: date = Field(...)
     horizon: int = Field(default=7, gt=0)
+
+    model_config = {"json_schema_extra": {"examples": [EXAMPLE_INPUT]}}
