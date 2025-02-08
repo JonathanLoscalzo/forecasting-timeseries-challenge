be39e96e5b241c15b622428d333e0178bd2fd4d7
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2025-02-08 14:00:16 -0300

    test: use parametrize and add integration as maker

--- forecasting/tests/clients/api_test.py
+++ forecasting/tests/clients/api_test.py
@@ -8,16 +8,26 @@ client = TestClient(app)
 
 
 @pytest.mark.integration
-def test_example_forecast_expected():
-    result = client.post("/forecast/", json=EXAMPLE_INPUT)
+@pytest.mark.parametrize(
+    ("input_data", "expected_status_code", "expected_result"),
+    [
+        (
+            EXAMPLE_INPUT,
+            200,
+            [
+                {"date": "2024-05-10", "value": 11.0},
+                {"date": "2024-05-11", "value": 10.75},
+                {"date": "2024-05-12", "value": 15.25},
+                {"date": "2024-05-13", "value": 9.8},
+                {"date": "2024-05-14", "value": 4.0},
+                {"date": "2024-05-15", "value": 11.0},
+                {"date": "2024-05-16", "value": 8.0},
+            ],
+        )
+    ],
+)
+def test_example_forecast_expected(input_data, expected_status_code, expected_result):
+    result = client.post("/forecast/", json=input_data)
 
-    assert result.status_code == 200
-    assert result.json() == [
-        {"date": "2024-05-10", "value": 11.0},
-        {"date": "2024-05-11", "value": 10.75},
-        {"date": "2024-05-12", "value": 15.25},
-        {"date": "2024-05-13", "value": 9.8},
-        {"date": "2024-05-14", "value": 4.0},
-        {"date": "2024-05-15", "value": 11.0},
-        {"date": "2024-05-16", "value": 8.0},
-    ]
+    assert result.status_code == expected_status_code
+    assert result.json() == expected_result
